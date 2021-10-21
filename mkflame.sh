6270c5a2ece29fef833c93a897f793b7849e22dd
Colin Percival <cperciva@tarsnap.com>
2021-10-20 23:10:20 -0700

    Improve flamechart
    Version number now appears in the title.
    
    Total boot time now appears as subtitle.
    
    Width of flamechart is scaled to one pixel = 20 ms, for easier
    comparison between charts.

--- mkflame.sh
+++ mkflame.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+TSCSTART=`sysctl -n debug.tslog | head -1 | cut -f 2 -d ' '`
+TSCEND=`sysctl -n debug.tslog_user | grep sh | head -1 | cut -f 4 -d ' '`
+TSCFREQ=`sysctl -n machdep.tsc_freq`
+
+MS=$(((TSCEND - TSCSTART) * 1000 / TSCFREQ));
+
 sh tslog.sh |
     perl stackcollapse-tslog.pl |
-    perl flamechart.pl -flamechart -colors tslog
+    perl flamechart.pl -flamechart -colors tslog --hash \
+	--title "`uname -r | cut -f 1-2 -d -` boot" \
+	--subtitle "$MS ms" \
+	--width $((MS / 20))
